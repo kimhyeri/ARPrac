b372de9c325949359cc22a674ce2ef77e45c1ff0
kimhyeri <hyer1k@naver.com>
2018-10-30 01:15:57 +0900

    Add gesture recognizer

--- AR/BallViewController.swift
+++ AR/BallViewController.swift
@@ -26,23 +26,10 @@ class BallViewController: UIViewController {
         sceneView.scene = scene
         
         addBack()
+        
+        registerGestureRecognizer()
     }
     
-    
-    func addBack() {
-        guard let backScene = SCNScene(named: "art.scnassets/hoop.scn") else {
-            return
-        }
-        
-        guard let backNode = backScene.rootNode.childNode(withName: "backboard", recursively: false) else {
-            return
-        }
-        
-        backNode.position = SCNVector3(0, 0.5, -3)
-        
-        sceneView.scene.rootNode.addChildNode(backNode)
-        
-    }
 
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -65,4 +52,48 @@ class BallViewController: UIViewController {
 
 extension BallViewController : ARSCNViewDelegate {
     
+    func registerGestureRecognizer() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        sceneView.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap(gestureRecognizer: UIGestureRecognizer) {
+
+        //scene view to be accessed
+        
+        //access the point of view of the scene view center point
+        
+        //access sceneView
+        guard let sceneView = gestureRecognizer.view as? ARSCNView else {
+            return
+        }
+        
+        guard let centerPoint = sceneView.pointOfView else {
+            return
+        }
+        
+        //transform matric
+        //the orientation
+        //the location fo camera
+        
+        
+        
+        
+    }
+    
+    func addBack() {
+        guard let backScene = SCNScene(named: "art.scnassets/hoop.scn") else {
+            return
+        }
+        
+        guard let backNode = backScene.rootNode.childNode(withName: "backboard", recursively: false) else {
+            return
+        }
+        
+        backNode.position = SCNVector3(0, 0.5, -3)
+        
+        sceneView.scene.rootNode.addChildNode(backNode)
+        
+    }
+
 }
